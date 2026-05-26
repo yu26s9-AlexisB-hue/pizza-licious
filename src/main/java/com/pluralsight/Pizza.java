e2db6d71d57225c9651c64211b57c0b3cf863295
Alexis Barrett <happynessb09@gmail.com>
2026-05-26 10:06:06 -0400

    Just finished a code review with my instructor. Shifting gears

--- src/main/java/com/pluralsight/Pizza.java
+++ src/main/java/com/pluralsight/Pizza.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 import ToppingEnums.CrustType;
 import ToppingEnums.PizzaSize;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pizza {
@@ -11,10 +12,39 @@ public class Pizza {
     private boolean isStuffedCrust;
     private List<Toppings> toppings;
 
-    public Pizza(PizzaSize size, CrustType crustChoice, boolean isStuffedCrust, List<Toppings> toppings) {
+    public Pizza(PizzaSize size, CrustType crustChoice, boolean isStuffedCrust) {
         this.size = size;
         this.crustChoice = crustChoice;
         this.isStuffedCrust = isStuffedCrust;
-        this.toppings = toppings;
+        this.toppings = new ArrayList<>();
+    }
+
+    public PizzaSize getSize() {
+        return size;
+    }
+
+    public void setSize(PizzaSize size) {
+        this.size = size;
+    }
+
+    public CrustType getCrustChoice() {
+        return crustChoice;
+    }
+
+    public void setCrustChoice(CrustType crustChoice) {
+        this.crustChoice = crustChoice;
+    }
+
+    public boolean isStuffedCrust() {
+        return isStuffedCrust;
+    }
+
+    public void setStuffedCrust(boolean stuffedCrust) {
+        isStuffedCrust = stuffedCrust;
+    }
+
+    public double calculatingPrice(){
+        //todo:Figure out how much the pizza costs just using the crust and size.
+        return 0;
     }
 }
